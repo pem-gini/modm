ff2ad2fdab991f6a65a36e6ec99c0eca5811d5e7
kikass13 <nick.fiege@ducktrain.io>
2022-09-23 16:06:29 +0200

    reset old mcp implementation and old spi dma impl

--- src/modm/driver/can/mcp2515_impl.hpp
+++ src/modm/driver/can/mcp2515_impl.hpp
@@ -4,7 +4,6 @@
  * Copyright (c) 2010, Thorsten Lajewski
  * Copyright (c) 2012-2015, 2017-2018, Niklas Hauser
  * Copyright (c) 2014, 2017, Sascha Schade
- * Copyright (c) 2023, Nick Fiege
  *
  * This file is part of the modm project.
  *
@@ -97,6 +96,7 @@ bool
 modm::Mcp2515<SPI, CS, INT>::initialize()
 {
 	using Timings = modm::CanBitTimingMcp2515<externalClockFrequency, bitrate>;
+
 	return initializeWithPrescaler(Timings::getPrescaler(), Timings::getSJW(), Timings::getProp(),
 								   Timings::getPS1(), Timings::getPS2());
 }
