701db99a0b24561383f77bebc62560cbb30a23d6
kikass13 <nick.fiege@ducktrain.io>
2022-11-23 17:41:03 +0100

    just throwing things in

---
+++ src/modm/architecture/driver/atomic/dma_ringbuffer.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <array>
+#include <cstddef>
+
+namespace modm
+{
+namespace atomic
+{
+
+template <class T, size_t SIZE> class DmaRxRingBuffer {
+public:
+  T &front() { return buf.at(tail); }
+  void pop() {
+    if (tail == SIZE - 1) {
+      tail = 0;
+    } else {
+      tail++;
+    }
+  }
+  bool read(T *target, size_t length) {
+    if (length > SIZE) {
+      return false;
+    }
+    if ((tail + length) > SIZE) {
+      size_t s1 = SIZE - tail;
+      size_t s2 = length - s1;
+      T *target_mid = target + s1;
+      std::memcpy(target, &buf.at(tail), s1);
+      std::memcpy(target_mid, &buf.at(0), s2);
+    } else {
+      std::memcpy(target, &buf.at(tail), length);
+    }
+    auto tmptail = tail + length;
+    if (tmptail >= (SIZE + 1)) {
+      tmptail = tmptail - SIZE;
+    }
+    tail = tmptail;
+    return true;
+  }
+  std::array<T, SIZE> buf;
+  T tail = 0;
+};
+
+}  // namespace atomic
+}  // namespace modm
