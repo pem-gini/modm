dcbe7080c982e582cf7f2fe4f40e79347fd41973
kikass13 <nick.fiege@ducktrain.io>
2022-11-25 17:31:25 +0100

    workign uartdmareceive

--- src/modm/architecture/driver/atomic/dma_ringbuffer.hpp
+++ src/modm/architecture/driver/atomic/dma_ringbuffer.hpp
@@ -8,46 +8,85 @@ namespace modm
 namespace atomic
 {
 
-template <class T, size_t SIZE> class DmaRxRingBuffer {
+class DmaRingBuffer
+{
 public:
-  static constexpr size_t maxSize(){ return SIZE; }
-  T &front() { return buf.at(tail); }
-  void pop() {
-    if (tail == SIZE - 1) {
-      tail = 0;
-    } else {
-      tail++;
-    }
-  }
-  
-  T at(auto index) const {
-    return buf.at(index);
-  }
-  size_t getReadIndex() const { return tail; }
-  const T* data() const { return buf.data(); }
-  bool read(T *target, size_t length) {
-    if (length > SIZE) {
-      return false;
-    }
-    if ((tail + length) > SIZE) {
-      size_t s1 = SIZE - tail;
-      size_t s2 = length - s1;
-      T *target_mid = target + s1;
-      std::memcpy(target, &buf.at(tail), s1);
-      std::memcpy(target_mid, &buf.at(0), s2);
-    } else {
-      std::memcpy(target, &buf.at(tail), length);
-    }
-    auto tmptail = tail + length;
-    if (tmptail >= (SIZE + 1)) {
-      tmptail = tmptail - SIZE;
-    }
-    tail = tmptail;
-    return true;
-  }
-  private:
-  std::array<T, SIZE> buf;
-  T tail = 0;
+	virtual size_t
+	maxSize() = 0;
+	virtual uint8_t
+	at(size_t index) const = 0;
+	virtual size_t
+	getIndex() const = 0;
+	virtual const uint8_t*
+	data() const = 0;
+	virtual bool
+	read(uint8_t* target, size_t length) = 0;
+	virtual size_t
+	getBytesWritten(size_t writeIndex) = 0;
+};
+
+template<class T, size_t SIZE>
+class DmaRxRingBuffer : public DmaRingBuffer
+{
+public:
+	size_t
+	maxSize()
+	{
+		return SIZE;
+	}
+	uint8_t
+	at(size_t index) const
+	{
+		return buf.at(index);
+	}
+	size_t
+	getIndex() const
+	{
+		return tail;
+	}
+	const uint8_t*
+	data() const
+	{
+		return reinterpret_cast<const uint8_t*>(buf.data());
+	}
+	bool
+	read(uint8_t* target, size_t length)
+	{
+		if (length > SIZE) { return false; }
+		if ((tail + length) > SIZE)
+		{
+			size_t s1 = SIZE - tail;
+			size_t s2 = length - s1;
+			T* target_mid = target + s1;
+			std::memcpy(target, &buf.at(tail), s1);
+			std::memcpy(target_mid, &buf.at(0), s2);
+		} else
+		{
+			std::memcpy(target, &buf.at(tail), length);
+		}
+		auto tmptail = tail + length;
+		if (tmptail >= (SIZE + 1)) { tmptail = tmptail - SIZE; }
+		tail = tmptail;
+		return true;
+	}
+	size_t
+	getBytesWritten(size_t writeIndex)
+	{
+		size_t bytesWritten = 0;
+		size_t r = getIndex();
+		if (writeIndex < r)
+		{
+			bytesWritten = maxSize() - r + writeIndex;
+		} else
+		{
+			bytesWritten = writeIndex - r;
+		}
+		return bytesWritten;
+	}
+
+private:
+	std::array<uint8_t, SIZE> buf;
+	T tail = 0;
 };
 
 }  // namespace atomic
